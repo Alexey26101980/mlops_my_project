0a895c3f22ea6546b76eb19aa130a95b9da68777
alexey <alexey@data-srv>
2022-11-04 20:46:11 +0300

    last changes

---
+++ scripts/data_scripts/get_data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Комментарий из VS Code
+
+from catboost.datasets import titanic
+
+train, test = titanic()
+
+train.to_csv("data/raw/train.csv", index=False)
+
+test.to_csv("data/raw/test.csv", index=False)
