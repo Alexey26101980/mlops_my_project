108d9b2fdc39de505ba22d05de25a592790e73e1
alexey <alexey@ml-srv>
2022-11-05 10:55:08 +0300

    Corrected get_data.py

--- scripts/data_scripts/get_data.py
+++ scripts/data_scripts/get_data.py
@@ -6,6 +6,6 @@ from catboost.datasets import titanic
 
 train, test = titanic()
 
-train.to_csv("data/raw/train.csv", index=False)
+train.to_csv("/home/alexey/projects/mlops_my_project/data/raw/train.csv", index=False)
 
-test.to_csv("data/raw/test.csv", index=False)
+test.to_csv("/home/alexey/projects/mlops_my_project/data/raw/test.csv", index=False)
